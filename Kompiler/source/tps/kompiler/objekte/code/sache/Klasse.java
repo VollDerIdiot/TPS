251fe7ba5b4a8b1e0a1159f879dba8d79d25899e
Patrick <Patrick@Coruscant>
2020-09-17 22:46:03 +0200

    macht und bessert hinzugefügt

--- Kompiler/source/tps/kompiler/objekte/code/sache/Klasse.java
+++ Kompiler/source/tps/kompiler/objekte/code/sache/Klasse.java
@@ -1,13 +1,15 @@
 package tps.kompiler.objekte.code.sache;
 
+import java.util.Collection;
+
 import tps.kompiler.objekte.code.Datentyp;
 import tps.kompiler.objekte.konstanten.Implementierungstiefe;
 import tps.kompiler.objekte.konstanten.Sichtbarkeit;
 
 public class Klasse extends Sache {
 	
-	public Klasse(Datentyp name, Implementierungstiefe impl, Sichtbarkeit sichtbarkeit) {
-		super(name, impl, sichtbarkeit);
+	public Klasse(Datentyp name, Implementierungstiefe impl, Sichtbarkeit sichtbarkeit, Datentyp bessert, Collection <Datentyp> macht) {
+		super(name, impl, sichtbarkeit, bessert, macht);
 	}
 	
 }
