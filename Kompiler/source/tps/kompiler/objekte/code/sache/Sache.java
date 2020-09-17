64768553633ab602ebd22137029b27f1800fcd54
Patrick <Patrick@Coruscant>
2020-09-17 22:45:03 +0200

    bessert und macht hinzugefügt

--- Kompiler/source/tps/kompiler/objekte/code/sache/Sache.java
+++ Kompiler/source/tps/kompiler/objekte/code/sache/Sache.java
@@ -1,9 +1,12 @@
 package tps.kompiler.objekte.code.sache;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Objects;
+import java.util.TreeSet;
 
 import tps.kompiler.objekte.code.Datentyp;
 import tps.kompiler.objekte.code.Methode;
@@ -16,20 +19,26 @@ public abstract class Sache {
 	public final Datentyp datentyp;
 	public final Implementierungstiefe impl;
 	public final Sichtbarkeit sichtbarkeit;
+	public final Datentyp bessert;
+	public final NavigableSet <Datentyp> macht;
 	protected List <Variable> variablen;
 	protected List <Methode> methoden;
 	
 	
 	
-	public Sache(Datentyp name, Implementierungstiefe impl, Sichtbarkeit sichtbarkeit) {
+	public Sache(Datentyp name, Implementierungstiefe impl, Sichtbarkeit sichtbarkeit, Datentyp bessert, Collection <Datentyp> macht) {
 		Objects.requireNonNull(name, "Der Name darf nicht null sein!");
 		Objects.requireNonNull(impl, "Die implementierungstiefe darf nicht null sein!");
 		Objects.requireNonNull(sichtbarkeit, "Die sichtbarkeit darf nicht null sein!");
+		Objects.requireNonNull(bessert, "bessert darf nicht null sein!");
+		Objects.requireNonNull(macht, "macht darf nicht null sein!");
 		this.datentyp = name;
 		this.impl = impl;
 		this.sichtbarkeit = sichtbarkeit;
 		variablen = new ArrayList <Variable>();
 		methoden = new ArrayList <Methode>();
+		this.bessert = bessert;
+		this.macht = Collections.unmodifiableNavigableSet(new TreeSet <Datentyp>(macht));
 	}
 	
 	
