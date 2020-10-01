1514fcdd07ff806f5a72c36dfb38c18218f83f19
Patrick <Patrick@Coruscant>
2020-10-01 13:24:27 +0200

    macht Methoden von CharSequence durch: Umleitung zu ZeichenKette Methode

---
+++ Zeichensatzt/src/de/hechler/patrick/patischerZeichensatzt/zeichenKette/UnfertigeZeichenKette.java
@@ -0,0 +1,24 @@
+package de.hechler.patrick.patischerZeichensatzt.zeichenKette;
+
+
+public abstract class UnfertigeZeichenKette implements ZeichenKetteInterface {
+	
+	@Override
+	public int length() {
+		return größe();
+	}
+	
+	@Override
+	public char charAt(int index) {
+		return getZeichen(index).getZeichen();
+	}
+	
+	@Override
+	public CharSequence subSequence(int start, int ende) {
+		return unterKette(start, ende);
+	}
+	
+	@Override
+	public abstract ZeichenKetteInterface clone();
+	
+}
