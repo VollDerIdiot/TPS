af3584ec8f21ecc345e90211f305a6c0d9d39805
feri <feri@HECHLERSPC3.Speedport_W_724V_01011603_07_002>
2020-10-07 21:11:37 +0200

    additional tests for TpstSourceLader.

--- Kompiler/testsrc/tps/kompiler/objekte/kompilieren/sourcenladen/TpstSourceLaderTest.java
+++ Kompiler/testsrc/tps/kompiler/objekte/kompilieren/sourcenladen/TpstSourceLaderTest.java
@@ -1,19 +1,24 @@
 package tps.kompiler.objekte.kompilieren.sourcenladen;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import tps.kompiler.objekte.fehler.FalscheSourcenFehler;
 import tps.kompiler.objekte.hilfen.Leser;
+import tps.kompiler.objekte.konstanten.Sichtbarkeit;
 import tps.kompiler.objekte.programm.Datentyp;
 
 
@@ -57,17 +62,39 @@ class TpstSourceLaderTest {
 		assertNotNull(dat.zusatzsachen);
 		assertTrue(dat.zusatzsachen.isEmpty());
 		
+		// Zusatzgedings:
+		//   von [Name] (Potentiell: ( + [Name])* und [Name])
 		sourceLader.sourceLeser = new Leser(new Scanner("variableEins von Zeichenkette"));
 		dat = sourceLader.leseDatentyp();
 		assertNotNull(dat);
 		assertEquals("variableEins", dat.name);
 		assertNotNull(dat.zusatzsachen);
-		System.out.println(dat.zusatzsachen);
+		Set<String> names = dat.zusatzsachen.stream().map(dt -> dt.name).collect(Collectors.toSet());
+		assertEquals(1, names.size());
+		assertTrue(names.contains("Zeichenkette"));
+
+		sourceLader.sourceLeser = new Leser(new Scanner("variableEins von Zeichenkette + variableZwei und Zahl"));
+		dat = sourceLader.leseDatentyp();
+		assertNotNull(dat);
+		assertEquals("variableEins", dat.name);
+		assertNotNull(dat.zusatzsachen);
+		names = dat.zusatzsachen.stream().map(dt -> dt.name).collect(Collectors.toSet());
+		assertEquals(3, names.size());
+		assertTrue(names.contains("Zeichenkette"));
+		assertTrue(names.contains("variableZwei"));
+		assertTrue(names.contains("Zahl"));
 	}
 	
 	@Test
-	void testLeseSichtbarkeit() {
-		fail("Not yet implemented");
+	void testLeseSichtbarkeit() throws FalscheSourcenFehler {
+		List<String> sichtbarkeitenKeywords = Arrays.asList("offen", "vererbe", "datei", "eigen");
+		for (String keyword:sichtbarkeitenKeywords) {
+			sourceLader.sourceLeser = new Leser(new Scanner(keyword));
+			Sichtbarkeit sichtbarkeit = sourceLader.leseSichtbarkeit();
+			assertNotNull(sichtbarkeit);
+			assertEquals(keyword, sichtbarkeit.name());
+		}
+		
 	}
 	
 	@Test
