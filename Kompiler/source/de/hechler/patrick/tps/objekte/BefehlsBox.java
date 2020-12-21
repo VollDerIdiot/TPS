77f60712983c4e5d1b977dbf3f9d28e9954e717d
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2020-12-21 22:35:23 +0100

    change

--- Kompiler/source/de/hechler/patrick/tps/objekte/BefehlsBox.java
+++ Kompiler/source/de/hechler/patrick/tps/objekte/BefehlsBox.java
@@ -1,6 +1,33 @@
 package de.hechler.patrick.tps.objekte;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import de.hechler.patrick.tps.objekte.befehle.Befehl;
 
 public class BefehlsBox {
+	
+	private List <Befehl> befehle;
+	
+	
+	public BefehlsBox() {
+		befehle = new ArrayList <>();
+	}
+	
+	
+	
+	public void neu(Befehl befehl) {
+		Objects.requireNonNull(befehl, "neuer Befehl");
+		befehle.add(befehl);
+	}
+	
+	public Befehl von(int index) {
+		return befehle.get(index);
+	}
+	
+	public int anzahl() {
+		return befehle.size();
+	}
 	
 }
--- Kompiler/source/de/hechler/patrick/tps/objekte/methode/Methode.java
+++ Kompiler/source/de/hechler/patrick/tps/objekte/methode/Methode.java
@@ -1,6 +1,23 @@
 package de.hechler.patrick.tps.objekte.methode;
 
+import java.util.Objects;
+
+import de.hechler.patrick.tps.objekte.BefehlsBox;
 
 public class Methode {
+	
+	public final MethodenKopf kopf;
+	public final BefehlsBox befehle;
+	
+	
+	
+	public Methode(MethodenKopf kopf, BefehlsBox befehle) {
+		this.kopf = Objects.requireNonNull(kopf, "kopf der Methode");
+		this.befehle = befehle == null ? new BefehlsBox() : befehle;
+	}
+	
+	public Methode(MethodenKopf kopf) {
+		this(kopf, null);
+	}
 	
 }
