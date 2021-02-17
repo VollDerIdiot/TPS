4e55f8fb5ee7b343c7d5e90c5d338158dba29801
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-02-17 14:17:53 +0100

    noch mehr Tests!

--- TPS-Interpreter/testsrc/de/hechler/patrick/tps/interpreter/impl/TpsInterpreterImplTest.java
+++ TPS-Interpreter/testsrc/de/hechler/patrick/tps/interpreter/impl/TpsInterpreterImplTest.java
@@ -23,6 +23,9 @@ class TpsInterpreterImplTest {
 	private static final String TEST_1_2 = "";
 	private static final String TEST_1_3 = "gebe das ergebnis AUS.";
 	private static final String TEST_2 = "addiere 4 mit 3. multipliziere 5 mit ergebnis. gebe folgendes aus: das ergebnis ist. gebe das ergebnis aus.";
+	private static final String TEST_3 = "addiere ergebnis mit 3.\r\n" + "speichere das ergebnis im zwischenspeicher.\r\n" + "mache einen zeilenumbruch.\r\n"
+			+ "multipliziere 5 mit ergebnis.\r\n" + "gebe den zwischenspeicher aus.\r\n" + "dividiere ergebnis mit zwischen. "
+			+ "mache einen zeilenumbruch. gebe das ergebnis aus.";
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -48,6 +51,15 @@ class TpsInterpreterImplTest {
 		TpsInterpreterImpl.instanz().interpretiere(new ByteArrayInputStream(TEST_2.getBytes(new Pzs8bCharset())), new Pzs8bCharset());
 		String printed = new String(out.toByteArray());
 		assertEquals("das ergebnis ist 35", printed);
+	}
+	
+	@Test
+	void testInterpretiere3() throws IOException, InterpretierungsFehler {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		TpsInterpreterImpl.instanz().interpretiere(new ByteArrayInputStream(TEST_3.getBytes(new Pzs8bCharset())), new Pzs8bCharset());
+		String printed = new String(out.toByteArray());
+		assertEquals(System.lineSeparator() + "3" + System.lineSeparator() + "5", printed);
 	}
 	
 }
