05d800c92eedfd61507edb72bbc99473cddfba73
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-02-18 21:28:14 +0100

    FehlersuchenInterpreter javadoc

--- TPS-Interpreter/src/de/hechler/patrick/tps/interpreter/FehlersuchenInterpreter.java
+++ TPS-Interpreter/src/de/hechler/patrick/tps/interpreter/FehlersuchenInterpreter.java
@@ -28,14 +28,53 @@ public interface FehlersuchenInterpreter extends Interpreter {
 	
 	int leseRegister(int register);
 	
-	AnordnungInterface aktuelles() throws NoSuchElementException;
+	int anzahlAnordnungen();
 	
+	void springeZu(int ziel);
+	
+	void rufeAuf(int ziel);
+	
+	/**
+	 * führt sofort einen geheZurück Befehl aus und gibt die {@link AnordnungInterface} zurück, zu welcher zurückgesprungen wurde.
+	 * 
+	 * @return die Anordnung zu welcher zu zurückgesprungen wurde.
+	 */
+	AnordnungInterface erzwingeZurückgehen();
+	
+	/**
+	 * gibt die Anordnung, welche als nächstes ausgeführt wird zurück.
+	 * 
+	 * @return die Anordnung, welche als nächstes ausgeführt wird.
+	 * @throws NoSuchElementException
+	 *             wenn bereits alles ausgeführt wurde.
+	 */
 	AnordnungInterface nächstes() throws NoSuchElementException;
 	
+	/**
+	 * führt eine Anordnung aus und gibt die {@link #nächstes()} Anordnung zurück.
+	 * 
+	 * @return die Anordnung, welche als nächstes ausgeführt wird
+	 * @throws IllegalStateException
+	 *             wenn bereits alles ausgeführt wurde.
+	 */
 	AnordnungInterface macheSchritt() throws IllegalStateException;
 	
+	/**
+	 * führt so lange alle Anordnungen durch, bis ein gehe zurück durchgeführt wird oder ein Stopper aktiviert wird. Dann wird getoppt und die {@link #nächstes()} Anordnung wird zurückgegeben.
+	 * 
+	 * @return die Anordnung, welche als nächstes ausgeführt wird
+	 * @throws IllegalStateException
+	 *             wenn bereits alles ausgeführt wurde.
+	 */
 	AnordnungInterface macheBisRaus() throws IllegalStateException;
 	
+	/**
+	 * führt so lange alle Anordnungen durch, bis ein Stopper aktiviert wird. Dann wird getoppt und die {@link #nächstes()} Anordnung wird zurückgegeben.
+	 * 
+	 * @return die Anordnung, welche als nächstes ausgeführt wird
+	 * @throws IllegalStateException
+	 *             wenn bereits alles ausgeführt wurde.
+	 */
 	AnordnungInterface macheBisStopper() throws IllegalStateException;
 	
 }
